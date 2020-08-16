71426a3bc78afa0c7a6a63caacca0384a6e8bedf
Airic <epineda773@protonmail.com>
2020-08-15 20:54:31 -0500

    Add files via upload
    todolist files (in the process of connecting it to a database)

--- app.js
+++ app.js
@@ -1,152 +1,55 @@
 //jshint esversion:6
 
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-//mongoose.connect("mongodb://localhost:27017/fruitsDB");
-mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
+const app = express();
 
-//New fruit Schema
-const fruitSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please check your data entry, no name specified!"]
-  },
-  rating: {
-    type: Number,
-    min: 1,
-    max: 10
-  },
-  review: String
-});
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-const Fruit = mongoose.model("Fruit", fruitSchema);
+//implementing mongoDB
+//Connect to mongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-const fruit = new Fruit({
+//New items Schema
+const itemsSchema = {
+  name: String
+};
+// end items Schema
 
-  rating: 10,
-  review: "Peaches are so yummy!"
-});
+//create new documents for database
+ const Item = mongoose.model("Item", itemsSchema);
 
-//fruit.save();
-//end fruit Schema
+ const item1 = new Item({
+   name: "Welcome to your todolist!"
+ });
 
-//New person Schema
-const personSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  favoriteFruit: fruitSchema
-});
+ const item2 = new Item({
+   name: "Hit the + button to add a new item."
+ });
 
-const Person = mongoose.model("Person", personSchema);
-//End new person Schema
+ const item3 = new Item({
+   name: "<--- hit this to delete an item."
+ });
 
-//start Pineapple
-const pineapple = new Fruit({
-  name: "Pineapple",
-  score: 9,
-  review: "Great fruit."
-});
-// //pineapple.save();
-//end Pineapple
+ const defaultItems = [item1, item2, item3];
+//end document creation
 
-//start Amy
-// const person = new Person({
-//   name: "Amy",
-//   age: 18,
-//   favoriteFruit: pineapple
-// });
-//End Amy
-
-//start Plum
-const plum = new Fruit({
-  name: "Plum",
-  score: 10,
-  review: "Good Stuff."
-});
-// plum.save();
-//End Plum
-
-//Start John
-// const person = new Person({
-//   name: "John",
-//   age: 37,
-//   favoriteFruit: plum
-// });
-// person.save();
-//End John
-
-//no longer in use, commented out for reference
-//add more than one fruit to database at one time
-// const kiwi = new Fruit({
-//   name: "Kiwi",
-//   score: 10,
-//   review: "The best fruit!"
-// });
-//
-// const orange = new Fruit({
-//   name: "orange",
-//   score: 10,
-//   review: "Too sour for me"
-// });
-//
-// const banana = new Fruit({
-//   name: "Banana",
-//   score: 3,
-//   review: "Weird texture"
-// });
-
-// Fruit.insertMany( [kiwi, orange, banana], function(err){
-//   if (err) {
-//     console.log(err);
-//   } else{
-//     console.log("Successfully saved all the fruits to fruitsDB");
-//   }
-// });
-//End insert many code block
-
-
-Fruit.find(function(err, fruits){
-  if(err){
-    console.log(err);
-  } else{
-
-    mongoose.connection.close();
-
-    fruits.forEach(function(fruit){
-      console.log(fruit.name);
-    });
-  }
-});
-
-//Update method
-// Fruit.updateOne({_id: "5f371f0a0f1937216c69abc1"}, {name: "Orange"}, function(err){
+//Start insertMany method
+// Item.insertMany(defaultItems, function(err){
 //   if(err){
 //     console.log(err);
 //   } else{
-//     console.log("Successfully updated the document.");
-//   }
+//     console.log("Successfully saved default items to DB.");
+//   };
 // });
+//end insertMany method
 
-//How to change John's favroite fruit through update method (I changed it above instead)
-Person.updateOne({name: "John"}, {favoriteFruit: plum}, function(err){
-  if(err){
-    console.log(err);
-  }else{
-    console.log("Successfully updated the document");
-  }
-});
-//End Update Method
-
-// Delete method
-// Fruit.deleteOne({ _id: "5f3754a085c65f2a94524593"}, function (err) {
-//   if(err){
-//     console.log(err);
-//   } else{
-//     console.log("Successfully deleted");
-//   }
-// });
-
-// Person.deleteOne({ _id: "5f37519df51f790444d737a3"}, function (err) {
+//start delete method
+// Item.deleteOne({ _id: "5f38900876ef2b43600ff078"}, function (err) {
 //   if(err){
 //     console.log(err);
 //   } else{
@@ -154,26 +57,36 @@ Person.updateOne({name: "John"}, {favoriteFruit: plum}, function(err){
 //   }
 // });
 // End Delete method
+//end mongoDB implementation
 
-//deleteMany Method
-// Person.deleteMany({ name: "John"}, function (err) {
-//   if(err){
-//     console.log(err);
-//   } else{
-//     console.log("Successfully deleted all the documents");
-//   }
-// });
-//End deleteMany Method
+//rest of original code
+app.get("/", function(req, res) {
 
-//Rest of the code
-const findDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Find some documents
-  collection.find({}).toArray(function(err, fruits) {
-    assert.equal(err, null);
-    console.log("Found the following records");
-    console.log(fruits)
-    callback(fruits);
-  });
-}
+  res.render("list", {listTitle: "Today", newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+
+  const item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
