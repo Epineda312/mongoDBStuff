7359467967c2fd9323b81b2971cc7821d08686fe
Airic <epineda773@protonmail.com>
2020-08-19 19:03:56 -0500

    Add files via upload
    add news items to the custom toDoList p2

--- app.js
+++ app.js
@@ -90,15 +90,24 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res){
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item({
     name: itemName
   });
 
-  item.save();
-  res.redirect("/");
-});
+  if(listname === "Today"){
+    item.save();
+    res.redirect("/");
+  } else {
 
+    List.findOne({name: listName}, function(err, foundList){
+    foundList.items.push(item);
+    foundList.save();
+    res.redirect("/" + listName);
+    });
+  }
+});
 //delete
 app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
